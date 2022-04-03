43a53a387b0b077109e181087a729f545c2d8a05
crismgsp <crismgsp@gmail.com>
2022-04-03 14:15:30 -0300

    mudanca no arquivo detalhaprodutos0controller

--- controller/detalhaProdutos-controller.js
+++ controller/detalhaProdutos-controller.js
@@ -34,43 +34,8 @@ const render = async () => {
     }
     catch(erro) {
         console.log(erro)
-        window.location.href = '../telas/erro.html' 
+        /*window.location.href = '../telas/erro.html' */
     }
 }
 
 render()
-
-
-
-
-
-
-/* daqui pra baixo depois vou tentar inserir a funcionalidade de produtos similares...pra esta acho que vou usar este forEach e vou ter 
-que dar um jeito de fazer uma verificacao antes do for pra que os elementos sejam da mesma categoria que o produto detalhado
-
-const tabela = document.querySelector('[data-categoriaproduto]')
-
-
-
-const render = async () => {
-    try{
-        const listaProdutos = await produtoService.listaProdutos()
-
-        listaProdutos.forEach(elemento => {
-            tabela.appendChild(criaNovaLinha(elemento.categoria, elemento.nome,elemento.preco, elemento.id ))
-        })
-    }
-    catch(erro) {
-        console.log(erro)
-        window.location.href = '../telas/erro.html'
-    }
-}
-
-render()
-
-
-
-
-
-
-
