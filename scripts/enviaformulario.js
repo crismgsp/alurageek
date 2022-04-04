423c0179c5002f2170e0f0ec2cd940d5772d5a00
crismgsp <crismgsp@gmail.com>
2022-04-04 14:36:45 -0300

    enviando js com tentativa de enviar formulario..falta ajustes

---
+++ scripts/enviaformulario.js
@@ -0,0 +1,50 @@
+const init = () => {
+
+    const verificaNomeeMensagem = (evento) => {
+        const input = evento.currentTarget;
+        if ((inputNome < 5) && inputMensagem < 10) {
+            submitButton.setAttribute('disabled', 'disabled');
+        }
+        else{
+            submitButton.removeAttribute('disabled', 'disabled');
+        }
+        
+    }
+
+    
+    const inputNome = document.querySelector('[data-nomeform]')
+    const inputMensagem = document.querySelector('[data-mensagem]')
+    const submitButton = document.querySelector('[data-botaoenvia]')
+
+    inputNome.addEventListener('input', verificaNomeeMensagem);
+
+    inputMensagem.addEventListener('input', verificaNomeeMensagem);
+
+    if (submitButton) {
+        submitButton.addEventListener('click', (evento) => {
+           evento.preventDefault();
+           
+            fetch(`https://my-json-server.typicode.com/crismgsp/backendformulario/mensagens` , {
+                method: 'POST',
+                headers: {
+                    'Content-type' : 'application/json'
+                },
+                body: JSON.stringify ({
+                    nome: inputNome.value,
+                    mensagem: inputMensagem.value
+                })
+            }).then (( resposta) => {  
+                if(resposta.ok){
+                    return resposta.json();
+                }               
+                throw new Error('Não foi possível adicionar o produto')
+       
+                })
+        })    
+
+    }
+
+}
+
+window.onload = init;
+
